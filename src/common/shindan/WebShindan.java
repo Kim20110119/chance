807fe90c1b9a26309fd9854461fb7a0e19abd19d
kimC <kim20110119@gmail.com>
2017-04-11 02:00:24 +0900

    スピードアップ

--- src/common/shindan/WebShindan.java
+++ src/common/shindan/WebShindan.java
@@ -3,6 +3,7 @@ package common.shindan;
 import static common.Common.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -17,7 +18,6 @@ public class WebShindan {
 
 	private static final String C_SUBMINT = "submit-btn";
 	private static final String C_END = "end-btn";
-	private static final String C_BTN = "btn-primary";
 
 	/**
 	 * =================================================================================================================
@@ -33,37 +33,45 @@ public class WebShindan {
 	 *
 	 */
 	public static Boolean execute(WebDriver driver) throws Exception {
+		/** JavaScript */
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
 		// 「次へ」
 		driver.findElement(By.className(C_SUBMINT)).click();
 		// 2秒待ち
-		Thread.sleep(2000);
+		Thread.sleep(1500);
 		// 「次へ」
 		driver.findElement(By.className(C_SUBMINT)).click();
 		// 1秒待ち
 		Thread.sleep(1000);
 		// 「診断質問」
+		executor.executeScript("$('#group-2').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-3').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-4').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-5').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-6').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-7').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-8').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-9').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-10').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-11').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-12').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-13').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-14').toggleClass('dia-invisible');");
+		executor.executeScript("$('#group-15').toggleClass('dia-invisible');");
 		for (int line = 1; line <= 15; line++) {
-			String div_id = "group-" + line;
-			String str_xpath = "//div[@id='" + div_id + "']/label[@class='label']";
-			int choices_count = driver.findElements(By.xpath(str_xpath)).size();
-			if (choices_count > 0) {
-				driver.findElements(By.xpath(str_xpath)).get(int_random(choices_count)).click();
-				if (line == 15) {
-					// 「終了」
-					driver.findElement(By.className(C_END)).click();
-					break;
-				} else {
-					// 「次へ」
-					driver.findElement(By.className(C_BTN)).click();
-				}
-			} else {
+			String value = driver.findElements(By.name("q"+line)).get(int_random(4)).getAttribute("value");
+			executor.executeScript("$('input[name=q"+ line +"]').val(['"+value+"']);");
+			if (line == 15) {
+				// 「終了」
+				executor.executeScript("$('#current').val('15');");
+				executor.executeScript("$('#form').submit();");
 				break;
 			}
 		}
 		// 「ポイント獲得」
 		driver.findElement(By.className(C_END)).click();
 		// 2秒待ち
-		Thread.sleep(2000);
+		Thread.sleep(1500);
 		// 「アラート」
 		driver.switchTo().alert().accept();
 		// 「診断処理結果」
